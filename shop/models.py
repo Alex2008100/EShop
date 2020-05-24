15070402eaabc767aeba946be73cc5fb5680d4b0
Human0001 <new7753142a@gmail.com>
2020-05-24 14:39:54 +0300

    Got some things per product page: title, price(s), description & more

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    brand = models.CharField(max_length=50, default="")
+    description = models.TextField(default="")
+    introduction = models.TextField(default="")
+    features = models.TextField(default="")
+    price_buy = models.IntegerField()
+    price_sell = models.IntegerField()
+    price_sell_no_discount = models.IntegerField()
+    sku = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
